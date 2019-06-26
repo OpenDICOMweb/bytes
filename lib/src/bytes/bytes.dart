294ff21cdca17f8fe7302effc2e4f226e41ee5e8
Jim Philbin <jfphilbin@users.noreply.github.com>
2019-06-26 13:50:23 -0400

    Remove offset and length from string setters.

--- lib/src/bytes/bytes.dart
+++ lib/src/bytes/bytes.dart
@@ -165,8 +165,8 @@ abstract class Bytes extends ListBase<int>
   }
 
   /// Creates an [Int8List] view of the specified region of _this_.
-  Int8List asInt8List([int offset = 0, int length]) => buf.buffer
-      .asInt8List(buf.offsetInBytes + offset, length ??= buf.length);
+  Int8List asInt8List([int offset = 0, int length]) =>
+      buf.buffer.asInt8List(buf.offsetInBytes + offset, length ??= buf.length);
 
   /// Returns an 8-bit _unsigned_ integer value
   /// (between 0 and 255 inclusive) at index [i].
@@ -321,54 +321,37 @@ abstract class Bytes extends ListBase<int>
   // **** String Setters
 
   // TODO: unit test
+  // TODO: what is the use case for having offset and length?
   /// Ascii encodes the specified range of [s] and then writes the
   /// code units to _this_ starting at [start].
-  ///
-  /// If [padChar] is not
-  /// _null_ and [s].length is odd, then [padChar] is written after
-  /// the code units of [s] have been written.
-  int setAscii(int start, String s, [int offset = 0, int length]) =>
-      _setStringBytes(start, cvt.ascii.encode(s), offset, length);
+  /// Returns the number of bytes written.
+  int setAscii(int start, String s) =>
+      _setStringBytes(start, cvt.ascii.encode(s));
 
   /// Writes the ASCII [String]s in [sList] to _this_ starting at
-  /// [start].
-  ///
-  /// If [padChar] is not _null_ and the final offset is odd,
-  /// then [padChar] is written after the other elements have been written.
-  /// Returns the number of bytes written.
-  int setAsciiList(int start, List<String> sList,
-          [int offset = 0, int length]) =>
-      _setLatinList(start, sList, offset, length, 127);
+  /// [start]. Returns the number of bytes written.
+  int setAsciiList(int start, List<String> sList, [String separator = '\\']) =>
+      _setLatinList(start, sList, separator, 127);
 
   // TODO: unit test
   /// UTF-8 encodes the specified range of [s] and then writes the
-  /// code units to _this_ starting at [start]. Returns the offset
-  /// of the last byte + 1.
-  int setLatin(int start, String s, [int offset = 0, int length]) =>
-      _setStringBytes(start, cvt.latin1.encode(s), offset, length);
+  /// code units to _this_ starting at [start].
+  /// Returns the number of bytes written.
+  int setLatin(int start, String s) =>
+      _setStringBytes(start, cvt.latin1.encode(s));
 
   /// Writes the LATIN [String]s in [sList] to _this_ starting at
-  /// [start]. If [padChar] is not _null_ and the final offset is odd,
-  /// then [padChar] is written after the other elements have been written.
-  /// Returns the number of bytes written.
+  /// [start]. Returns the number of bytes written.
+  ///
   /// _Note_: All latin character sets are encoded as single 8-bit bytes.
-  int setLatinList(int start, List<String> sList,
-          [int offset = 0, int length]) =>
-      _setLatinList(start, sList, offset, length, 255);
+  int setLatinList(int start, List<String> sList, [String separator = '\\']) =>
+      _setLatinList(start, sList, separator, 255);
 
-  /// Copy [String]s from [sList] into _this_ separated by backslash.
-  /// If [padChar] is not equal to _null_ and last character position
-  /// is odd, then add [padChar] at end.
-  // Note: this only works for ascii or latin
+  /// Copy [String]s from [sList] into _this_ separated by [separator].
+  // Note: this only works for single byte code points, e.g. Ascii, Latin...).
   int _setLatinList(
-    int start,
-    List<String> sList,
-    int offset,
-    int length,
-    int limit,
-  ) {
-    const _kBackslash = 92;
-//    assert(padChar == _kSpace || padChar == _kNull);
+      int start, List<String> sList, String separator, int limit) {
+    final sepChar = separator.codeUnitAt(0);
     if (sList.isEmpty) return 0;
     final last = sList.length - 1;
     var k = start;
@@ -381,50 +364,28 @@ abstract class Bytes extends ListBase<int>
           throw ArgumentError('Character code $c is out of range $limit');
         setUint8(k++, s.codeUnitAt(j));
       }
-      if (i != last) setUint8(k++, _kBackslash);
+      if (i != last) setUint8(k++, sepChar);
     }
-//    if (k.isOdd && padChar != null) setUint8(k++, padChar);
     return k - start;
   }
 
   // TODO: unit test
   /// UTF-8 encodes [s] and then writes the code units to _this_
-  /// starting at [start]. Returns the offset of the last byte + 1.
+  /// starting at [start]. Returns the number of bytes written.
   int setUtf8(int start, String s) =>
-      _setStringBytes(start, cvt.utf8.encode(s), 0, null);
+      _setStringBytes(start, cvt.utf8.encode(s));
 
   /// Converts the [String]s in [sList] into a [Uint8List].
   /// Then copies the bytes into _this_ starting at
-  /// [start].
-  ///
-  /// If [padChar] is not _null_ and the offset of the last
-  /// byte written is odd, then [padChar] is written to _this_.
-  /// Returns the number of bytes written.
+  /// [start]. Returns the number of bytes written.
   int setUtf8List(int start, List<String> sList, [String separator = '\\']) =>
       setUtf8(start, sList.join(separator));
 
-/*
-  /// Moves bytes from [list] to _this_. If [list].[length] is odd adds [pad]
-  /// as last byte. Returns the number of bytes written.
-  int _setStringBytes(int start, Uint8List list,
-      [int offset = 0, int length, int pad = _kSpace]) {
-    length ??= list.length;
-    for (var i = offset, j = start; i < length; i++, j++) buf[j] = list[i];
-    if (length.isOdd && pad != null) {
-      final end = offset + length;
-      if (end > buf.length) throw ArgumentError();
-      buf[end] = pad;
-      return length + 1;
-    }
-    return length;
-  }
-*/
-
   /// Moves bytes from [list] to _this_. Returns the number of bytes written.
-  int _setStringBytes(int start, Uint8List list, [int offset = 0, int length]) {
-    length ??= list.length;
-    for (var i = offset, j = start; i < length; i++, j++) buf[j] = list[i];
-    return length;
+  int _setStringBytes(int start, Uint8List list) {
+    final len = list.length;
+    for (var i = 0, j = start; i < len; i++, j++) buf[j] = list[i];
+    return len;
   }
 
   // TODO fix to use Latin
@@ -433,9 +394,8 @@ abstract class Bytes extends ListBase<int>
   /// of the last byte + 1.
   ///
   /// Note: Currently only encodes Latin1.
-  void setString(int start, String s,
-          [Encoder encoder, int offset = 0, int length]) =>
-      _setStringBytes(start, encoder(s), offset, length);
+  void setString(int start, String s, [Encoder encoder]) =>
+      _setStringBytes(start, encoder(s));
 
   // *** Comparable interface
 
